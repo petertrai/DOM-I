680c4fbe64f22eb9565c09952580bf829f8779cb
petertrai <petertrai@gmail.com>
2023-05-17 11:13:19 -0700

    finish project

--- src/index.js
+++ src/index.js
@@ -36,7 +36,61 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
+    
   },
 };
+// Images
+const logoImage = document.querySelector('#logo-img');
+logoImage.src = siteContent.images['logo-img'];
+const ctaImage = document.querySelector('#cta-img');
+ctaImage.src = siteContent.images["cta-img"]
+const midImage = document.querySelector('#middle-img');
+midImage.src = siteContent.images['accent-img']
 
-console.log('project wired!')
+// Footer Link
+const footerLink = document.querySelector('footer a');
+footerLink.classList.add('bold') 
+footerLink.textContent = siteContent.footer.copyright;
+
+// Contact
+const contact = document.querySelector('section.contact');
+contact.querySelector('h4').textContent = siteContent.contact['contact-h4']
+contact.querySelector('p:nth-of-type(1)').textContent = siteContent.contact.address
+contact.querySelector('p:nth-of-type(2)').textContent = siteContent.contact.phone
+contact.querySelector('p:nth-of-type(3)').textContent = siteContent.contact.email
+
+// Top Content
+// 1st child / div
+const topContent = document.querySelector('.top-content');
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4']
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content']
+// 2nd child / div
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4']
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content']
+
+// Bottom Content 
+// 1st child/div
+const bottomContent = document.querySelector('.bottom-content');
+bottomContent.children[0].children[0].textContent = siteContent['main-content']['services-h4']
+bottomContent.children[0].children[1].textContent = siteContent['main-content']['services-content']
+// 2nd child / div
+bottomContent.children[1].children[0].textContent = siteContent['main-content']['product-h4']
+bottomContent.children[1].children[1].textContent = siteContent['main-content']['product-content']
+// 3rd child / div 
+bottomContent.children[2].children[0].textContent = siteContent['main-content']['vision-h4']
+bottomContent.children[2].children[1].textContent = siteContent['main-content']['vision-content']
+
+// CTA 
+const cta = document.querySelector('.cta-text')
+console.log(cta)
+cta.querySelector('h1').textContent = siteContent.cta.h1
+cta.querySelector('button').textContent = siteContent.cta.button
+
+//nav
+const nav = document.querySelector('nav');
+nav.children[0].textContent = siteContent.nav['nav-item-1']
+nav.children[1].textContent = siteContent.nav['nav-item-2']
+nav.children[2].textContent = siteContent.nav['nav-item-3']
+nav.children[3].textContent = siteContent.nav['nav-item-4']
+nav.children[4].textContent = siteContent.nav['nav-item-5']
+nav.children[5].textContent = siteContent.nav['nav-item-6']
